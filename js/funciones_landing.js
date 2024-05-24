eb9e8717121a617ff8838be0fe078b0a3b4f6c94
MarcoFlores2022 <48026343+marcoflores95@users.noreply.github.com>
2024-05-24 12:05:27 -0600

    v1

---
+++ js/funciones_landing.js
@@ -0,0 +1,11 @@
+function alumnos() {
+    window.location.href = "https://login.microsoftonline.com/seggtogob.onmicrosoft.com/oauth2/authorize?response_type=code&client_id=76983198-657a-4380-8d8b-1296f31b3cc8&scope=openid%20profile%20email&nonce=N6650bd7a3c7e5&response_mode=form_post&state=0cDZgsNFqoHIk8B&redirect_uri=https%3A%2F%2Fformat.seg.guanajuato.gob.mx%2Festudiantes%2Fauth%2Foidc%2F&resource=https%3A%2F%2Fgraph.microsoft.com";
+}
+
+function docentes() {
+    window.location.href = "https://format.seg.guanajuato.gob.mx/menu_estudiantes/index.html";
+}
+
+function contacto() {
+    window.location.href = "mailto:pil@seg.guanajuato.gob.mx";
+}
